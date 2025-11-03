6e8139239cf9567ecb0e9c4f9603d93b21d5fa7f
Yurik <hural1205@gmail.com>
2025-11-03 18:16:34 +0200

    master

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../АП laba 5.1/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+			Assert::AreEqual(k(1, 1),0.6909,0.05);
+		}
+	};
+}
